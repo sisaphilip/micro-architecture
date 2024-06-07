f8fffaac93c26dff57249baf7dcef9e03634c7be
sisaphilip <sisaphilip@gmail.com>
2024-06-07 16:41:45 +0300

    9/10

--- 05_05_a_plus_b_using_fifos_and_double_buffer/flip_flop_fifo_with_counter.sv
+++ 05_05_a_plus_b_using_fifos_and_double_buffer/flip_flop_fifo_with_counter.sv
@@ -1,4 +1,3 @@
-// Example
 module flip_flop_fifo_with_counter
 # (
     parameter width = 8, depth = 10
@@ -14,22 +13,21 @@ module flip_flop_fifo_with_counter
     output               full
 );
 
-//------------------------------------------------------------------------
-// $clog2 for minimum address width that can cover memory size
-    localparam pointer_width = $clog2 (depth),    
-               counter_width = $clog2 (depth + 1);  
-               
+    //------------------------------------------------------------------------
+
+    localparam pointer_width = $clog2 (depth),
+               counter_width = $clog2 (depth + 1);
+
     localparam [counter_width - 1:0] max_ptr = counter_width' (depth - 1);
 
-//------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
     logic [pointer_width - 1:0] wr_ptr, rd_ptr;
     logic [counter_width - 1:0] cnt;
 
     logic [width - 1:0] data [0: depth - 1];
 
-
-//------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
     always_ff @ (posedge clk or posedge rst)
         if (rst)
@@ -43,7 +41,7 @@ module flip_flop_fifo_with_counter
         else if (pop)
             rd_ptr <= rd_ptr == max_ptr ? '0 : rd_ptr + 1'b1;
 
-//------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
     always_ff @ (posedge clk)
         if (push)
@@ -51,7 +49,7 @@ module flip_flop_fifo_with_counter
 
     assign read_data = data [rd_ptr];
 
-//------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
     always_ff @ (posedge clk or posedge rst)
         if (rst)
@@ -61,10 +59,9 @@ module flip_flop_fifo_with_counter
         else if (pop & ~ push)
             cnt <= cnt - 1'b1;
 
-//------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
-
-    assign empty = (cnt == '0);  // Same as "~| cnt"
-    assign full = (cnt == depth);
+    assign empty = ( cnt == '0    );  // Same as "~| cnt"
+    assign full  = ( cnt == depth );
 
 endmodule
